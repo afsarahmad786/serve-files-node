522bf77276d0d3afcb351e8ed6b0b2d54d3002bd
afsarahmad786 <mohammadafsar415@gmail.com>
2022-12-04 11:13:00 +0530

    Serve Files HTML CSS JS

---
+++ routes/shop.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
+});
+
+router.get("/contactus", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "contact.html"));
+});
+
+router.post("/success", (req, res, next) => {
+  console.log("success");
+  res.sendFile(path.join(rootDir, "views", "success.html"));
+});
+
+module.exports = router;
